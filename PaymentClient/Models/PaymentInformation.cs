078972bc1bfb336e31345dcd334c3219e8250bf1
Vikram Jhurry <vikram.jhurry@gmail.com>
2020-01-19 15:36:33 +0400

    addd project add razor pages and model and setup navigation menu. Connect to API

---
+++ PaymentClient/Models/PaymentInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentClient.Models
+{
+    public class PaymentInformation
+    {
+        
+        public string CardNo { get; set; }
+        public string Expiry { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+        public string CVV { get; set; }
+
+    }
+}
---
+++ PaymentClient/Pages/Payments/MakePayments.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PaymentClient.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace PaymentClient
+{
+    public class MakePaymentsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+
+        [BindProperty]
+
+        public PaymentInformation paymentInformation { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            HttpClient httpClient = new HttpClient();
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(paymentInformation), Encoding.UTF8, "application/json");
+            await httpClient.PostAsync("https://localhost:44382/api/payment", content);
+           
+            return RedirectToPage("../Index");
+        }
+
+    }
+}
